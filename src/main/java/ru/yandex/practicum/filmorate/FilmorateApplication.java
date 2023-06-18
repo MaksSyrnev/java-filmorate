be1badea381fa9bf955d55cc22965f2a63950dec
MaksSyrnev <msyrnev@yandex.ru>
2023-06-18 11:09:11 +1000

    codstyle-bugfix

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
-import com.sun.tools.javac.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FilmorateApplication {
