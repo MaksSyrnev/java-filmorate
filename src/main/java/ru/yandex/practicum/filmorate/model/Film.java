255f0d9f79c71ee59f32861dfaf2c1fd02c98f58
MaksSyrnev <msyrnev@yandex.ru>
2023-08-03 23:04:35 +1000

    checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
